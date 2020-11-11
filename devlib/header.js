836479067946462ad124e006d80382d139754c66
80LK <80lkru@gmail.com>
2020-11-11 19:29:06 +0300

    Initial commit

---
+++ devlib/header.js
@@ -0,0 +1,43 @@
+/*
+     ____             _            _____ _ _       _____       _   _ _         _     _ _     
+    |  _ \  ___   ___(_)_ __   ___|_   _(_) | ___ | ____|_ __ | |_(_) |_ _   _| |   (_) |__  
+    | |_) |/ _ \ / __| | '_ \ / _ \ | | | | |/ _ \|  _| | '_ \| __| | __| | | | |   | | '_ \ 
+    |  _ <|  __/| (__| | |_) |  __/ | | | | |  __/| |___| | | | |_| | |_| |_| | |___| | |_) | 
+    |_| \_\\___| \___|_| .__/ \___| |_| |_|_|\___||_____|_| |_|\__|_|\__|\__, |_____|_|_.__/ 
+                       |_|                                               |___/                     
+                                                                
+    RecipeTileEntityLib v3.0 ©WolfTeam ( https://vk.com/wolf___team )
+*/
+/*ChangeLog:
+    v.3.0
+        - Migrate TypeScript
+    v.2.1
+        - Fix errors
+        - Shared
+        - Integration with RecipeViewer
+        - Fix recipe with mod's items
+        - The setWorkbench method was added to the tileentity of the workbench
+    v.2.0
+        - The library has been rewritten
+        - Added the ability to create shapeless recipes
+    v.1.2
+        - Fix change of craft with the same ingredients
+        - Removed method registerTimerGridCraftTable
+        - Removed method registerTimerCraftTable
+        - Removed method getTickResipes
+    v.1.1
+        - Merge methods registerGridCraftTable and registerTimerGridCraftTable
+        - Merge methods registerCraftTable and registerTimerCraftTable
+        - Fixed method name from getTickResipes to getTickRecipes
+        - Added the ability to use your item IDs.
+        - For recipes, the time multiplier parameter has been added.
+    v.1
+        - release
+*/
+
+LIBRARY({
+    name: "RecipeTileEntityLib",
+    version: 30,
+    api: "CoreEngine",
+    shared:true
+});
